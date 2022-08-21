d96aa474b3da8219a7d65814ae4c7ba63f121cb4
Maxim <sibmaks.khv@mail.ru>
2022-08-21 16:55:36 +1000

    Add Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные 
+// элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] array1(int size, int leftSize, int rightSize)
+{
+    int[] arr = new int[size];
+
+    Random random = new Randome();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = random.Next(leftSize, rightSize+1);
+    }
+    return arr;
+}
